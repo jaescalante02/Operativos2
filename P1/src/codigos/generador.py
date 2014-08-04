aa2d0b01e2ad400a8705ac19548ec17d9f667105
Wilmer Bandres <wilmer0593@hotmail.com>
2014-08-04 12:17:38 -0430

    agregando generador en python

---
+++ P1/src/codigos/generador.py
@@ -0,0 +1,21 @@
+import sys
+import random
+
+casos = int(raw_input())
+print "<processes>"
+for i in range(0,casos):
+	print "\t<process>"
+	print "\t\t<id>",i,"</id>"
+	size = random.randint(1,45)
+	arrival = random.randint(500,10000)
+	print "\t\t<item>"
+	print "\t\t\t<arrival>",arrival,"</arrival>"
+	print "\t\t\t<size>",size,"</size>"
+	acs  = random.randint(1,20)
+	for k in range(0,acs):
+		tmp = random.randint(0,size-1)
+		print "\t\t\t<page>",tmp,"<page>"
+	print "\t\t</item>"
+	print "\t</process>"
+
+print "</processes>"
